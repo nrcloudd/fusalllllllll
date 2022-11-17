2fea1a3c16bdd0e56d338c31e8205e65f5df1efa
nrcloudd <116167202+nrcloudd@users.noreply.github.com>
2022-11-17 16:23:38 +0700

    login change

--- futsal/login.js
+++ futsal/login.js
@@ -1,11 +1,20 @@
-const signUpButton = document.getElementById('signUp');
-const signInButton = document.getElementById('signIn');
-const container = document.getElementById('container');
-
-signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
-});
-
-signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
-});
+$('.loginBtn').click(function(){
+	$('.login').show();
+	$('.signUp').hide();
+	/* border bottom on button click */
+	$('.loginBtn').css({'border-bottom' : '2px solid #ff4141'});
+	/* remove border after click */
+	$('.signUpBtn').css({'border-style' : 'none'});
+  });
+   
+   
+  /* Show sign Up form on button click */
+   
+  $('.signUpBtn').click(function(){
+	$('.login').hide();
+	$('.signUp').show();
+	/* border bottom on button click */
+	$('.signUpBtn').css({'border-bottom' : '2px solid #ff4141'});
+	 /* remove border after click */
+	 $('.loginBtn').css({'border-style' : 'none'});
+  });
