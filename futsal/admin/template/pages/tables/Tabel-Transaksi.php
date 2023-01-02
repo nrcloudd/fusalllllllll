1949b738b955e8ba16be2343ffd40922cc73c008
dfitriana28 <111998066+dfitriana28@users.noreply.github.com>
2023-01-02 10:24:02 +0700

    Update Tabel-Transaksi.php

--- futsal/admin/template/pages/tables/Tabel-Transaksi.php
+++ futsal/admin/template/pages/tables/Tabel-Transaksi.php
@@ -16,7 +16,7 @@ require ("koneksi.php");
   <!-- inject:css -->
   <link rel="stylesheet" href="../../css/style.css">
   <!-- endinject -->
-  <link rel="shortcut icon" href="../../images/logo.png" />
+  <link rel="shortcut icon" href="admin\template\pages\icons/logo1.png" />
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
