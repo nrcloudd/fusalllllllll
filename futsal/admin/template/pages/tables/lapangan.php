32dd2ac2b2f2de044620b2e0d97d0c164170d63b
dfitriana28 <111998066+dfitriana28@users.noreply.github.com>
2023-01-02 07:53:56 +0700

    Update lapangan.php

--- futsal/admin/template/pages/tables/lapangan.php
+++ futsal/admin/template/pages/tables/lapangan.php
@@ -6,7 +6,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>RoyalUI Admin</title>
+    <title>ZonaFutsal Admin</title>
     <!-- plugins:css -->
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.6.15/dist/sweetalert2.all.min.js"></script>
     <script src="sweetalert2.all.min.js"></script>
@@ -19,7 +19,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
     <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.1/css/jquery.dataTables.min.css">
-    <link rel="shortcut icon" href="../../images/favicon.png" />
+    <link rel="shortcut icon" href="../../images/logo.png" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
 </head>
 
