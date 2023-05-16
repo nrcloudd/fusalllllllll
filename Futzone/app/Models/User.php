620c259182928ca1d3f8b8d50dc1261186ed7f38
nrcloudd <116167202+nrcloudd@users.noreply.github.com>
2023-05-16 14:13:19 +0700

    API create

--- Futzone/app/Models/User.php
+++ Futzone/app/Models/User.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -20,7 +20,6 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'username',
         'password',
     ];
 
@@ -42,9 +41,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-    public function setPasswordAttribute($value){
-        $this->attributes['password'] = bcrypt($value);
-    }
- 
 }
-
