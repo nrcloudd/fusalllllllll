184d3cf44f82d78e64664f32930ece1f0ca49d6f
thoriqre <117810381+thoriqre@users.noreply.github.com>
2023-05-19 11:14:24 +0700

    Create basic-table.php

---
+++ Futzone/resources/views/Admin/basic-table.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <neta name="Viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title>RoyalUI Admin</title>
+      <!-- plugins:css -->
+      <link rel="stylesheet" href="../../vendors/ti-icons/css/themify-icons.css">
+      <link rel="stylesheet" href="../../vendors/base/vendor.bundle.base.css">
