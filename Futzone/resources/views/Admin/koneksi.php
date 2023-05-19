2c517007e4266fb7aafadb709578762bd570c599
thoriqre <117810381+thoriqre@users.noreply.github.com>
2023-05-19 11:04:03 +0700

    Update koneksi.php

--- Futzone/resources/views/Admin/koneksi.php
+++ Futzone/resources/views/Admin/koneksi.php
@@ -1,1 +1,10 @@
-
+<?php
+$server     = "localhost";
+$username   = "root";
+$password   = "";
+$db         = "newfutsal";
+$koneksi =mysqli_connect($server, $username, $password, $db);
+if (mysqli_connect_errno()){
+    echo "koneksi gagal : " . mysqli_connect_error()
+    }
+?>
