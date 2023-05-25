926e10101fa3cb02c88fa5f575b7f9801fcb98b9
thoriqre <117810381+thoriqre@users.noreply.github.com>
2023-05-25 07:59:10 +0700

    Update basic-table.php

--- Futzone/resources/views/Admin/basic-table.php
+++ Futzone/resources/views/Admin/basic-table.php
@@ -1,11 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
+<head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
-    <neta name="Viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-      <title>RoyalUI Admin</title>
-      <!-- plugins:css -->
-      <link rel="stylesheet" href="../../vendors/ti-icons/css/themify-icons.css">
-      <link rel="stylesheet" href="../../vendors/base/vendor.bundle.base.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>RoyalUI Admin</title>
+    <!-- plugins:css -->
+    <link rel="stylesheet" href="../../vendors/ti-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="../../vendors/base/vendor.bundle.base.css">
+    <!-- endinject -->
+    <!-- inject:css -->
+    <link rel="stylesheet" href="../../css/style.css">
+    <!-- endinject -->
+    <link rel="shortcut icon" href="../../images/favicon.png" />
+</head>
