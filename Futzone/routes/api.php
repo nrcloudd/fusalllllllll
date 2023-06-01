acc9e983756f07d30c92c4eb2f9f5953763068d3
nrcloudd <116167202+nrcloudd@users.noreply.github.com>
2023-06-01 09:43:52 +0700

    login register logout api

--- Futzone/routes/api.php
+++ Futzone/routes/api.php
@@ -1,21 +1,30 @@
 <?php
 
-use App\Http\Controllers\BlogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+/**
+ * route "/register"
+ * @method "POST"
+ */
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/**
+ * route "/login"
+ * @method "POST"
+ */
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+
+/**
+ * route "/user"
+ * @method "GET"
+ */
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::resource('blogs', BlogController::class);
+
+/**
+ * route "/logout"
+ * @method "POST"
+ */
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
--- Futzone/routes/web.php
+++ Futzone/routes/web.php
@@ -1,9 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\AdminController;
-use App\Http\Controllers\LoginController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,41 +13,6 @@ use App\Http\Controllers\LoginController;
 |
 */
 
-Route::group(['namespace' => 'App\Http\Controllers'], function()
-{
-    /**
-     * Home Routes
-     */
-    // Route::get('/', 'HomeController@index')->name('home.index');
-    Route::get('/basic', [AdminController::class,'basic']);
-    Route::get('/employe', [AdminController::class,'employe']);
-    Route::get('/lapangan', [AdminController::class,'lapangan']);
-    Route::get('/detailTransaksi', [AdminController::class,'detailTransaksi']);
-    Route::get('/tableregister', [AdminController::class,'tableregister']);
-    Route::get('/transaksi', [AdminController::class,'transaksi']);
-    Route::get('/dashboard', [AdminController::class,'dashboard']);
-    Route::get('/', [LoginController::class,'show']);
-    Route::get('/home', [HomeController::class,'index']);
-
-    Route::group(['middleware' => ['guest']], function() {
-        /**
-         * Register Routes
-         */
-        Route::get('/register', 'RegisterController@show')->name('register.show');
-        Route::post('/register', 'RegisterController@register')->name('register.perform');
-
-        /**
-         * Login Routes
-         */
-        Route::get('/login', 'LoginController@show')->name('login.show');
-        Route::post('/login', 'LoginController@login')->name('login.perform');
-
-    });
-
-    Route::group(['middleware' => ['auth']], function() {
-        /**
-         * Logout Routes
-         */
-        Route::get('/logout', 'LogoutController@perform')->name('logout.perform');
-    });
+Route::get('/', function () {
+    return view('welcome');
 });
