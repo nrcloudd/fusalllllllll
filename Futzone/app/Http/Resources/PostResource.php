43cbe878f2bb8f2c5217bf101ac77b101647dd47
1ayub1 <thorireformasi@gmail.com>
2023-06-06 20:21:49 +0700

    Create PostResource.php

---
+++ Futzone/app/Http/Resources/PostResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PostResource extends JsonResource
+{
+    //define properti
+    public $status;
+    public $message;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'success'   => $this->status,
+            'message'   => $this->message,
+            'data'      => $this->data];
+            parent::toArray($request);
+    }
+}
+return [
+    'success'   => $this->status,
+    'message'   => $this->message,
+    'data'      => $this->resource
+ ];
